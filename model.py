9317bf45d38930a9cbd68224a99dbf5839caa2dd
Yujin Lee <yujin.gha@gmail.com>
2017-11-25 20:57:13 +0900

    first submit version

--- model.py
+++ model.py
@@ -8,12 +8,12 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers import Convolution2D
 from sklearn.model_selection import train_test_split
 
 
-# In[2]:
+# In[24]:
 
 
 lines =[]
@@ -37,7 +37,7 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 
-# In[3]:
+# In[25]:
 
 
 def preprocess(img):
@@ -45,66 +45,65 @@ def preprocess(img):
     numOfImage = len(img)
     for i in range(0, numOfImage):
         img1 = img[i]
-        new_img = cv2.cvtColor(img1, cv2.COLOR_BGR2YUV)
+        new_img = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
         new_images.append(new_img)
     
     new_images = np.array(new_images)
     return new_images
 
 
-# In[ ]:
+# In[26]:
 
 
-def addFlippedData(img, measurement):
-    flipped_images=[]
-    numOfImage = len(img)
-    for i in range(0, numOfImage):
-        img1 = img[i]
-        image_flipped = np.fliplr(img1)
-        flipped_images.append(image_flipped)
-    
-    flipped_images= np.array(flipped_images)
-    
-    img.append(flipped_images)
-    
-    measurement_flipped = -measurement
-    measurement.append(measurement_flipped)
-    
-    return img, measurement
+def addFlippedData(images, measurements):
+    flipped_images, flipped_measurements = [],[]
+  
+    for image, measurement in zip(images, measurements):
+        flipped_images.append(image)
+        flipped_measurements.append(measurement)
+        flipped_images.append(cv2.flip(image, 1))
+        flipped_measurements.append(measurement * -1.0)
+        
+    X_augmented = np.array(flipped_images)
+    y_augmented = np.array(flipped_measurements)
+    return X_augmented, y_augmented
 
 
-# In[4]:
+# In[27]:
 
 
 
 X_train_proc = preprocess(X_train)
 
 
-# In[ ]:
+# In[28]:
 
 
+X_augmented, y_augmented = addFlippedData(X_train_proc, y_train)
+#X_augmented= X_train_proc
+#y_augmented = y_train
 
 
-
-# In[5]:
+# In[29]:
 
 
 import sklearn
 def splitData(features, labels, splitRatio =0.2):
 
     X_train, X_validation, y_train, y_validation = train_test_split(features, labels, test_size = splitRatio)
-    trainSet =[X_train, y_train]
-    validationSet = [X_validation, y_validation]
+    trainSet =sklearn.utils.shuffle(X_train, y_train)
+    validationSet = sklearn.utils.shuffle(X_validation, y_validation)
 
     return trainSet, validationSet
     
 
 
-# In[6]:
+# In[30]:
 
 
 model = Sequential()
-model.add(Lambda(lambda x: (x/255.0)- 0.5, input_shape =(160, 320, 3)))
+model.add(Cropping2D(cropping=((65,25), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x/255.0)- 0.5))
 model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
 model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
 model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
@@ -117,61 +116,66 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 
-# In[16]:
+# In[31]:
 
 
 def generator(samples, batch_size):
  # Create empty arrays to contain batch of features and labels#
     numFeatures = len(samples[0])
-    batch_features = np.zeros((batch_size, 160, 320, 3))
+    rowLen = np.shape(trainSet[0])[1]
+    colLen = np.shape(trainSet[0])[2]
+    batch_features = np.zeros((batch_size, rowLen, colLen, 3))
     batch_labels = np.zeros((batch_size,1))
     while True:
         for offset in range(0, numFeatures, batch_size):
             endIndex = min(offset+ batch_size, numFeatures)
             batch_features = (samples[0][offset:endIndex])
             batch_labels = (samples[1][offset:endIndex])
-        #for i in range(batch_size):
-            # choose random index in features
-         #   index= np.random.choice(len(features),1)
-            #batch_features[i] = some_processing(features[index])
-           # batch_features[i] = (features[index])
-           # batch_labels[i] = labels[index]
+
             yield [batch_features, batch_labels]
-
-
-# In[15]:
-
-
-trainSet, validationSet = splitData(X_train_proc,y_train)
 
 
 # In[ ]:
 
 
-#model = Sequential()
-#model.add(Flatten(input_shape=(160, 320, 3)))
-#model.add(Dense(1))
+
+
+
+# In[32]:
+
+
+trainSet, validationSet = splitData(X_augmented,y_augmented)
+
+
+# In[34]:
+
+
+
 batch_size = 32
 numTrain = len(trainSet[0])
 numValidation = len(validationSet[0])
 train_generator = generator(trainSet, batch_size)
 validation_generator = generator(validationSet, batch_size)
 model.compile(loss='mse', optimizer ='adam')
-#model.fit(X_train_proc, y_train, validation_split =0.2, shuffle = True)
-model_history = model.fit_generator(train_generator, samples_per_epoch=numTrain, nb_epoch=3)
+model_history = model.fit_generator(train_generator, samples_per_epoch=numTrain, validation_data = validation_generator, nb_val_samples =numValidation,  nb_epoch=5)
 model.save('model.h5')
 
 
-# In[ ]:
+# In[12]:
 
 
+#import matplotlib.pyplot as plt
+#%matplotlib inline
+#plt.plot(model_history.history['loss'])
 
 
-
-# In[ ]:
-
+# In[39]:
 
 
+#import matplotlib.pyplot as plt
+#%matplotlib inline
+#plt.imshow(cv2.flip(X_train_proc[0], 1))
+#plt.title("flipped image")
 
 
 # In[ ]:
